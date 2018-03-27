93301ecaf42fee79d9ecee6e9d9c4a16942d4dc9
Michelle Olivieri <michelleo72@gmail.com>
2018-03-27 13:57:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,10 +36,10 @@ class Song
 
   def self.find_or_create_by_name(string)
     if self.find_by_name == string
-      song
+      string
     else
       self.create_by_name
-      song
+      string
     end
   end
 
