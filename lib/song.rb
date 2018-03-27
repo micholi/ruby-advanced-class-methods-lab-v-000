bdaaaff52d8983db5d1d1754f15d70e21ca2e954
Michelle Olivieri <michelleo72@gmail.com>
2018-03-27 13:45:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,8 +30,8 @@ class Song
     song
   end
 
-  def self.find_by_name(name)
-    @@all.detect {|song| song.name == name}
+  def self.find_by_name(song_string)
+    @@all.detect {|song| song.song_string == song_string}
   end
 
   def self.find_or_create_by_name
