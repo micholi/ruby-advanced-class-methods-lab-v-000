82abd9696b67c896a16d4373165406f99c36964f
Michelle Olivieri <michelleo72@gmail.com>
2018-03-27 14:55:58 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,7 @@ class Song
   def self.new_from_filename(mp3_file)
     s = self.new
     s.name = File.basename(filename, ".mp3").split("-")
-
+    s
     #c = self.new
     #   c.name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[1]
     #   c.artist_name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[0]
